ac48290b220692db26134f13fe3572624b8e3c7b
Elsayed Rady <119011348+Elsayed-Rady-Elsayed@users.noreply.github.com>
2024-10-15 13:43:01 +0300

    Add files via upload

---
+++ LAPS/DAY1_LAP/Lap1Task2/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int Num;
+    printf("enter the number:");
+    scanf("%i",&Num);
+    printf("the hex value of the number is : %x",Num);
+    return 0;
+}
