7426658be15d698df39b9c2f3e657a247159c830
Elsayed Rady <119011348+Elsayed-Rady-Elsayed@users.noreply.github.com>
2024-10-17 01:30:16 +0300

    Update main.c

--- DAY2_LAP/Lap2Calc/main.c
+++ DAY2_LAP/Lap2Calc/main.c
@@ -4,44 +4,50 @@
 int main()
 {
     int num1,num2;
-    char op;
-    char continueWorking;
+    char op,EndLoop,extra;
     do{
-        printf("please enter the first number:");
-        _flushall();
-        scanf("%i",&num1);
-        printf("please enter the second number:");
-        _flushall();
-        scanf("%i",&num2);
+        char extra;
+        int res;
         do{
-            printf("please enter operation from (*,/,-,+,%):");
+            printf("enter the first number:");
+            _flushall();
+            res = scanf("%i%c",&num1,&extra);
+        }while((int)extra!=10);
+        do{
+            printf("enter the second number:");
+            _flushall();
+            res = scanf("%i%c",&num2,&extra);
+        }while((int)extra!=10);
+        do{
+             printf("enter the operation:");
             _flushall();
             scanf("%c",&op);
-        }while(op!='*'&&op!='/'&&op!='-'&&op!='+'&&op!='%');
+        }while(op!='%'&& op!='+'&&op!='-'&&op!='/'&&op!='*');
         int result;
         switch(op){
-            case '+':
-                result=num1+num2;
+        case '*':
+            result=num1*num2;
             break;
-            case '-':
-                result=num1-num2;
+        case '-':
+            result=num1-num2;
             break;
-            case '*':
-                result=num1*num2;
+        case '/':
+            result=num1/num2;
             break;
-            case '/':
-                result=num1/num2;
+        case '+':
+            result=num1+num2;
             break;
-            case '%':
-                result=num1%num2;
+        case '%':
+            result=num1%num2;
             break;
-            default:
-                result=0;
+        default:
+            result=0;
         }
         printf("%i %c %i = %i \n",num1,op,num2,result);
-        printf("continue? to exit press n \n");
+        printf("press n to end");
         _flushall();
-        scanf("%c",&continueWorking);
-    }while(continueWorking!='n');
+        scanf("%c",&EndLoop);
+    }while(EndLoop!='n');
+
     return 0;
 }
