d46890744bef7018ddf3e1af3b7fa900c2bbe7c6
Elsayed Rady <119011348+Elsayed-Rady-Elsayed@users.noreply.github.com>
2024-10-17 01:39:49 +0300

    Update main.c

--- DAY2_LAP/Lap2MagicBox/main.c
+++ DAY2_LAP/Lap2MagicBox/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
-
 void gotoxy( int column, int row )
 {
     COORD coord;
@@ -13,23 +12,18 @@ int main()
 {
     int size;
     do{
-        printf("enter size thats not even or equal 1:");
+        printf("enter the size of the magic box:");
         scanf("%i",&size);
     }while(size==1||size%2==0);
-    int row = 1,col = (size/2)+1;
+    int row = 1,col=(size/2)+1;
     system("cls");
-    for(int i = 1;i<=size*size;i++){
+    for(int i=1;i<=size*size;i++){
         gotoxy(col*3,row*3);
         printf("%i",i);
         if(i%size!=0){
-            row--;
-            col--;
-            if(row<1){
-                row=size;
-            }
-            if(col<1){
-                col=size;
-            }
+            row--;col--;
+            if(row<1){row=size;}
+            if(col<1){col=size;}
         }else{
             row++;
         }
