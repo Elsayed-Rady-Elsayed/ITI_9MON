15f2a2391708a415985d901b010e45dc7b96f962
Elsayed Rady <119011348+Elsayed-Rady-Elsayed@users.noreply.github.com>
2024-10-17 15:18:56 +0300

    Update main.c

--- DAY3_LAP/lap3CalcWithFunction/main.c
+++ DAY3_LAP/lap3CalcWithFunction/main.c
@@ -1,78 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
-int add(int,int);
-int sub(int,int);
-int dive(int,int);
-int mult(int,int);
-int main()
-{
-    int num1,num2;
-    char op,breakLoop;
-    do{
-        char extra;
+int add(int, int);
+int sub(int, int);
+int dive(int, int);
+int mult(int, int);
+
+int main() {
+    int num1, num2;
+    char op, breakLoop;
+
+    do {
         int res;
-        do{
-            printf("enter the first number:");
-            _flushall();
-            res = scanf("%i%c",&num1,&extra);
-        }while((int)extra!=10);
-
-        do{
-            printf("enter the second number:");
-            _flushall();
-            res = scanf("%i%c",&num2,&extra);
-        }while((int)extra!=10);
-
-        do{
-            printf("enter the operation:");
-            _flushall();
-            scanf("%c",&op);
-            if(op=='/'&&num2==0){
-               do{
-                printf("enter the second number that not 0:");
+        do {
+            printf("Enter the first number: ");
+            res = scanf("%d", &num1);
+            if (res != 1) {
+                printf("Invalid input. Please enter an integer.\n");
                 _flushall();
-                res = scanf("%i%c",&num2,&extra);
-                }while(num2==0);
             }
-        }while(op!='*'&&op!='+'&&op!='-'&&op!='/');
+        } while (res != 1);
+
+        do {
+            printf("Enter the second number: ");
+            res = scanf("%d", &num2);
+            if (res != 1) {
+                printf("Invalid input. Please enter an integer.\n");
+                _flushall();
+            }
+        } while (res != 1);
+
+        do {
+            printf("Enter the operation (+, -, *, /): ");
+            _flushall();
+            scanf("%c", &op);
+            if (op == '/' && num2 == 0) {
+                printf("Error: Division by zero is undefined.\n");
+            }
+        } while (op != '+' && op != '-' && op != '*' && op != '/' || (op == '/' && num2 == 0));
 
         int result = 0;
-        switch(op){
+        switch (op) {
             case '+':
-            result=add(num1,num2);
-            break;
+                result = add(num1, num2);
+                break;
             case '-':
-            result=sub(num1,num2);
-            break;
+                result = sub(num1, num2);
+                break;
             case '/':
-            result=dive(num1,num2);
-            break;
+                result = dive(num1, num2);
+                break;
             case '*':
-            result=mult(num1,num2);
-            break;
+                result = mult(num1, num2);
+                break;
             default:
-                result=0;
-                printf("your entered wrong operator");
+                printf("Invalid operator.\n");
         }
-
-        printf("%i %c %i = %i \n",num1,op,num2,result);
-
+        printf("%d %c %d = %d\n", num1, op, num2, result);
         printf("to exit press n otherwise press y:");
         _flushall();
-        scanf("%c",&breakLoop);
-    }while(breakLoop != 'n');
+        scanf("%c", &breakLoop);
+    } while (breakLoop != 'n');
 
     return 0;
 }
-int add(int n1,int n2){
-    return n1+n2;
+
+int add(int n1, int n2) {
+    return n1 + n2;
 }
-int sub(int n1,int n2){
-    return n1-n2;
+
+int sub(int n1, int n2) {
+    return n1 - n2;
 }
-int dive(int n1,int n2){
-    return n1/n2;
+
+int dive(int n1, int n2) {
+    return n1 / n2;
 }
-int mult(int n1,int n2){
-    return n1/n2;
+
+int mult(int n1, int n2) {
+    return n1 * n2;
 }
